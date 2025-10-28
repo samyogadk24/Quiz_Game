f6606d22cb46600a383f6fc53a88162d949604ec
samyogadk24 <samyogdbs19@gmail.com>
2025-10-28 18:04:30 +0545

    first commit

---
+++ Quizgame_project/src/main/java/com/example/Quizgame/Main.java
@@ -0,0 +1,11 @@
+package main.java.com.example.Quizgame;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        QuizController controller = new QuizController();
+        controller.startGame();
+    }
+}
+
