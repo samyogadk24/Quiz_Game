c2734de310f8e054ed2075779f96f3c5a4bf5763
samyogadk24 <samyogdbs19@gmail.com>
2025-10-29 23:35:16 +0545

    program

--- Quizgame_project/src/main/java/com/example/Quizgame/Main.java
+++ Quizgame_project/src/main/java/com/example/Quizgame/Main.java
@@ -1,11 +1,14 @@
 package main.java.com.example.Quizgame;
 
+
 public class Main {
 
     public static void main(String[] args) {
 
-        QuizController controller = new QuizController();
-        controller.startGame();
-    }
+System.out.println("Hello gamers! Welcome to my Quiz game ");
+
+    }   
 }
+
+
 
