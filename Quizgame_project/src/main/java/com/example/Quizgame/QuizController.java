cfb78eab7e38efe57990735ad5d8efec05041de2
samyogadk24 <samyogdbs19@gmail.com>
2025-10-30 21:35:22 +0545

    added QuizController.java

--- Quizgame_project/src/main/java/com/example/Quizgame/QuizController.java
+++ Quizgame_project/src/main/java/com/example/Quizgame/QuizController.java
@@ -1,13 +1,18 @@
 package main.java.com.example.Quizgame;
 
 
-public class Main {
+public class QuizController{
 
     public static void main(String[] args) {
 
 System.out.println("Hello gamers! Welcome to my Quiz game ");
+System.out.println("--------------------------------------- ");
 
-    }   
+ QuizController controller = new QuizController();
+
+ 
+
+}
 }
 
 
